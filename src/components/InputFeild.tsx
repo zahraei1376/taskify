fcc208311fca269bca23b2d14a3f8f350b7ccaed
zahra <zahra0a1418@gmail.com>
2023-12-06 14:42:37 +0330

    add todo to list

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -3,11 +3,12 @@ import React from "react";
 interface Props {
     value: string,
     setValue: React.Dispatch<React.SetStateAction<string>>,
+    handleAdd: (e: React.FormEvent) => void,
 }
 
-const InputFeild: React.FC<Props> = ({ value, setValue }) => {
+const InputFeild: React.FC<Props> = ({ value, setValue, handleAdd }) => {
     return (
-        <form className="flex items-center w-11/12">
+        <form className="flex items-center w-11/12" onSubmit={handleAdd}>
             <input type="text" id="task" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl shadow-2xl focus:ring-blue-500 focus:border-blue-500 outline-none block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="enter a task" required value={value} onChange={e => setValue(e.target.value)} />
             <button type="submit" className="bg-sky-500 rounded-full h-9 w-9 -ml-11 text-white text-sm shadow-lg">GO</button>
         </form>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import InputFeild from "../components/InputFeild";
+import { Todo } from "../Models";
 
 const Home: React.FC = () => {
     const [todo, setTodo] = useState<string>("");
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const handleAdd = (e: React.FormEvent): void => {
+        e.preventDefault();
+
+        if (todo) {
+            setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+        }
+        setTodo("");
+    }
 
     return (
         <div className="pt-5 w-full h-full bg-sky-500 flex flex-col items-center">
             <span className="font-bold text-center text-white">TASKIFY</span>
-            <InputFeild value={todo} setValue={setTodo} />
+            <InputFeild value={todo} setValue={setTodo} handleAdd={handleAdd} />
         </div>
     )
 }
