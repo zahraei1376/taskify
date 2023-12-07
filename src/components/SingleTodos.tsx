bc35c7aa2fc8ddb980d637e3788373a86a82e00b
zahra <zahra0a1418@gmail.com>
2023-12-07 10:52:31 +0330

    add handleDone for todo

--- src/components/SingleTodos.tsx
+++ src/components/SingleTodos.tsx
@@ -9,15 +9,35 @@ type Props = {
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>,
 }
 const SingleTodos = ({ todo, todos, setTodos }: Props) => {
+
+    const handleDone = (id: number) => {
+        setTodos(todos.map(todo => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo));
+    }
+
+    const handleDelete = (id: number) => {
+
+    }
+
+    const handleEdit = (id: number) => {
+
+    }
     return (
         <form className="rounded bg-amber-400 flex justify-between p-4 w-full sm:w-60 my-2">
-            <span className="text-white flex-wrap">
-                {todo.todo}
-            </span>
+            {
+                todo.isDone ?
+                    <s>
+                        {todo.todo}
+                    </s>
+                    :
+                    <span>
+                        {todo.todo}
+                    </span>
+            }
+
             <div className="flex">
-                <span className="cursor-pointer pr-1"><FaCheck /></span>
-                <span className="cursor-pointer pr-1"><MdDeleteForever /></span>
-                <span className="cursor-pointer pr-1"><MdEdit /></span>
+                <span className="cursor-pointer pr-1" onClick={() => handleDone(todo.id)}><FaCheck /></span>
+                <span className="cursor-pointer pr-1" onClick={() => handleDelete(todo.id)}><MdDeleteForever /></span>
+                <span className="cursor-pointer pr-1" onClick={() => handleEdit(todo.id)}><MdEdit /></span>
             </div>
         </form>
     )
